519f189898704470fb757148ad3150fc61be90e8
yangzifeiyu <yangzifeiyu@qq.com>
2016-07-21 14:08:01 +0800

    OpenTemplate and Save Template are defined in AbstractTemplateDesigner.

--- commons/src/main/java/com/mfusion/commons/controllers/AbstractTemplateDesigner.java
+++ commons/src/main/java/com/mfusion/commons/controllers/AbstractTemplateDesigner.java
@@ -3,15 +3,29 @@ package com.mfusion.commons.controllers;
 import android.content.Context;
 import android.widget.LinearLayout;
 
+import com.mfusion.commons.entity.template.TemplateEntity;
+
 /**
  * Created by guoyu on 2016/7/13.
  */
 public abstract class AbstractTemplateDesigner extends LinearLayout {
 
+    //private TemplateEntity m_template;
     public AbstractTemplateDesigner(Context context) {
         super(context);
         // TODO Auto-generated constructor stub
     }
 
-    public abstract Boolean openTemplate(String templateName);
+    /**
+     * Open a template entity
+     * @param template
+     * @return
+     */
+    public abstract Boolean OpenTemplate  (TemplateEntity template);
+
+    /***
+     *Save a template entity
+     * @return
+     */
+    public  abstract TemplateEntity SaveTemplate();
 }
