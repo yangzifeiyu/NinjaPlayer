e9756dd90c31010b4651ececcbe0dc7cca49507d
JCYYYY <icysomnus@gmail.com>
2016-08-05 13:51:39 +0800

    Update TemplateFragment.java

--- app/src/main/java/com/mfusion/ninjaplayer/FragmentClass/TemplateFragment.java
+++ app/src/main/java/com/mfusion/ninjaplayer/FragmentClass/TemplateFragment.java
@@ -1,112 +1,138 @@
 package com.mfusion.ninjaplayer.FragmentClass;
 
-import android.app.ProgressDialog;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.GridView;
+import android.widget.FrameLayout;
 
-import com.mfusion.commons.data.XMLTemplate;
-import com.mfusion.commons.entity.template.TemplateEntity;
 import com.mfusion.commons.entity.template.VisualTemplate;
 import com.mfusion.ninjaplayer.R;
-import com.mfusion.ninjaplayer.TemplateView2;
-import com.mfusion.ninjaplayer.view.TemplateGridViewAdapter;
-import com.mfusion.templatedesigner.EventDispatcher;
-import com.mfusion.templatedesigner.TemplateDesigner;
+import com.mfusion.ninjaplayer.view.TemplateDesigningView;
+import com.mfusion.ninjaplayer.view.TemplateFragmentListener;
+import com.mfusion.ninjaplayer.view.TemplateSampleView;
+import com.mfusion.ninjaplayer.view.TemplateUserCreatedView;
 
-import java.util.ArrayList;
-
-public class TemplateFragment extends Fragment implements View.OnClickListener{
+public class TemplateFragment extends Fragment{
     private static final String TAG = "TemplateView1";
 
-    private GridView gridView;
-    private TemplateDesigner designer;
-    private Button btnNew;
-    private Button btnEdit;
+    private FrameLayout placeHolder;
+
+    private TemplateUserCreatedView templateUserCreatedView;
+    private TemplateSampleView templateSampleView;
+    private TemplateDesigningView templateDesigningView;
+
+    private FlowListener flowListener;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        flowListener=new FlowListener();
 
-        final View rootView = inflater.inflate(R.layout.fragment_template, container, false);
+        templateSampleView=new TemplateSampleView(getContext());
+        templateSampleView.setListener(flowListener);
+        templateUserCreatedView=new TemplateUserCreatedView(getContext());
+        templateUserCreatedView.setListener(flowListener);
+        templateDesigningView=new TemplateDesigningView(getContext());
+        templateDesigningView.setListener(flowListener);
 
-        //supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
-        gridView = (GridView) rootView.findViewById(R.id.template_list_grid_view);//grid view
-        designer = (TemplateDesigner) rootView.findViewById(R.id.template_designer);
+        View rootView = inflater.inflate(R.layout.fragment_template,container,false);
+        placeHolder= (FrameLayout) rootView.findViewById(R.id.fragment_template_place_holder);
+        placeHolder.addView(templateUserCreatedView);
 
-        btnEdit = (Button) rootView.findViewById(R.id.template_btn_edit);//edit button
-
-
-
-        try {
-            ArrayList<VisualTemplate> visualTemplates= XMLTemplate.getInstance().getAllLocalSampleLayouts(getActivity().getAssets(),"template");
-            TemplateGridViewAdapter adapter=new TemplateGridViewAdapter(getActivity(),visualTemplates);//
-            gridView.setAdapter(adapter);
-            gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                    designer.clearComponentOnScreen();
-                    VisualTemplate visualTemplate=(VisualTemplate) gridView.getItemAtPosition(position);
-                    TemplateEntity selectedEntity= XMLTemplate.getInstance().getSampleLayout(visualTemplate,getActivity().getAssets());
-                    ProgressDialog progressDialog=ProgressDialog.show(getActivity(),null,"Loading...");
-                    EventDispatcher.registerProgressDialog(progressDialog);
-                    progressDialog.show();
-
-                    designer.openTemplate(selectedEntity);
-                    toggleListAndDesigner();
+        //View rootView=new TemplateSampleView(getContext());
 
 
+//        gridView = (GridView) rootView.findViewById(R.id.template_list_grid_view);
+//        designer = (TemplateDesigner) rootView.findViewById(R.id.template_designer);
+//
+//        btnSave = (Button) rootView.findViewById(R.id.template_btn_save);
+//        btnCancel=(Button)rootView.findViewById(R.id.template_btn_cancel);
+//
+//        btnSave.setOnClickListener(this);
+//        btnCancel.setOnClickListener(this);
+//
+//
+//
+//        try {
+//            ArrayList<VisualTemplate> visualTemplates= XMLTemplate.getInstance().getAllLocalSampleLayouts(getActivity().getAssets(),"template");
+//            TemplateGridViewAdapter adapter=new TemplateGridViewAdapter(getActivity(),visualTemplates);
+//            gridView.setAdapter(adapter);
+//            gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//                @Override
+//                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                    designer.clearComponentOnScreen();
+//                    VisualTemplate visualTemplate=(VisualTemplate) gridView.getItemAtPosition(position);
+//                    TemplateEntity selectedEntity= XMLTemplate.getInstance().getSampleLayout(visualTemplate,getActivity().getAssets());
+//                    ProgressDialog progressDialog=ProgressDialog.show(getActivity(),null,"Loading...");
+//                    EventDispatcher.registerProgressDialog(progressDialog);
+//                    progressDialog.show();
+//
+//                    designer.openTemplate(selectedEntity);
+//                    toggleListAndDesigner();
+//
+//
+//
+//                }
+//            });
+//        } catch (Exception e) {
+//            Log.e(TAG, "onCreate: ",e );
+//        }
+//
+//
+//        designer.setVisibility(View.GONE);
 
-                }
-            });
-        } catch (Exception e) {
-            Log.e(TAG, "onCreate: ",e );
-        }
 
-
-        designer.setVisibility(View.GONE);
-
-        btnNew = (Button) rootView.findViewById(R.id.template_btn_new);
-        btnNew.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                Intent intent = new Intent(getActivity(),TemplateView2.class);
-                startActivity(intent);
-                //finish();
-            }
-        });
 
         return  rootView;
     }
 
-    private void toggleListAndDesigner(){
-        if(designer.getVisibility()==View.GONE){
-            designer.setVisibility(View.VISIBLE);
-            gridView.setVisibility(View.GONE);
+    private class FlowListener implements TemplateFragmentListener{
+
+        @Override
+        public void goSampleView() {
+            placeHolder.removeAllViews();
+            placeHolder.addView(templateSampleView);
         }
-        else{
-            designer.setVisibility(View.GONE);
-            gridView.setVisibility(View.VISIBLE);
+
+        @Override
+        public void goUserCreatedView() {
+            placeHolder.removeAllViews();
+            templateUserCreatedView.refresh();
+            placeHolder.addView(templateUserCreatedView);
+        }
+
+        @Override
+        public void goDesigner(VisualTemplate selectedVisualTemplate) {
+            placeHolder.removeAllViews();
+            placeHolder.addView(templateDesigningView);
+            templateDesigningView.openTemplate(selectedVisualTemplate);
         }
     }
-    @Override
-    public void onClick(View v) {
-        if(v.getId()==R.id.template_btn_new){
-            toggleListAndDesigner();
-            //TODO: SAVE COMPONENT
-        }
-        else if(v.getId()==R.id.template_btn_edit){
-            toggleListAndDesigner();
-        }
-    }
+
+//    private void toggleListAndDesigner(){
+//        if(designer.getVisibility()==View.GONE){
+//            designer.setVisibility(View.VISIBLE);
+//            gridView.setVisibility(View.GONE);
+//        }
+//        else{
+//            designer.setVisibility(View.GONE);
+//            gridView.setVisibility(View.VISIBLE);
+//        }
+//    }
+//    @Override
+//    public void onClick(View v) {
+//        if(v.getId()==R.id.template_btn_save){
+//            toggleListAndDesigner();
+//            //TODO: SAVE COMPONENT
+//        }
+//        else if(v.getId()==R.id.template_btn_cancel){
+//            toggleListAndDesigner();
+//
+//
+//        }
+//    }
 }
