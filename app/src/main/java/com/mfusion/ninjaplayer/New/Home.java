b46650f3498f8b0df6e5b11e1a309b8a62310113
WangMengNi <hansey20@hotmail.com>
2016-08-05 09:28:27 +0800

    Update Home.java

--- app/src/main/java/com/mfusion/ninjaplayer/New/Home.java
+++ app/src/main/java/com/mfusion/ninjaplayer/New/Home.java
@@ -21,37 +21,38 @@ public class Home extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        go = (Button) findViewById(R.id.btnGo);
-        ins = (Button) findViewById(R.id.btnInt);
+        go = (Button) findViewById(R.id.btnGo);//proceed button
+        ins = (Button) findViewById(R.id.btnInt);//instruction button
 
 
         go.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent intObj = new Intent(Home.this,MainActivity.class);
+                Intent intObj = new Intent(Home.this,MainActivity.class);//intent from home class to mainActivity class
                 startActivity(intObj);
 
 
             }
-        });
+        });//go to mainActivity
+        
         ins.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Toast.makeText(Home.this, "You click instructions", Toast.LENGTH_SHORT).show();
+                Toast.makeText(Home.this, "You click instructions", Toast.LENGTH_SHORT).show();//display when clicking ins(instruction) button
                 AlertDialog alertDialog = new AlertDialog.Builder(Home.this).create();
-                alertDialog.setTitle("Instruction");
+                alertDialog.setTitle("Instruction");//set Title
                 alertDialog.setMessage("Step 1: Click Mfusion" + "\n"+ "\n" + "Step 2: Configure"+ "\n"+ "\n" + "Step 3: Choose templates"
                         + "\n"+ "\n" + "Step 4: Adjust Templates"+ "\n"+ "\n" + "Step 5: Create PBU" + " " + "Step 6: Schedule"
-                        + "\n"+ "\n" + "Step 7: RUN and Display"+ "\n"+ "\n" + "Step 8: Finish");
+                        + "\n"+ "\n" + "Step 7: RUN and Display"+ "\n"+ "\n" + "Step 8: Finish");//set dialog message (instruction)
                 alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
                         new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int which) {
-                                dialog.dismiss();
+                                dialog.dismiss();//dismiss dialog
                             }
                         });
-                alertDialog.show();
+                alertDialog.show();//show dialog
 
 
             }
