09701910d28acd63dffba0ec8a976037c5039d03
WangMengNi <hansey20@hotmail.com>
2016-08-05 09:22:55 +0800

    Update MainApplication.java

--- app/src/main/java/com/mfusion/ninjaplayer/MainApplication.java
+++ app/src/main/java/com/mfusion/ninjaplayer/MainApplication.java
@@ -4,9 +4,6 @@ import android.app.Application;
 
 import com.facebook.stetho.Stetho;
 
-/**
- * Created by 1B15182 on 21/7/2016 0021.
- */
 public class MainApplication extends Application {
     @Override
     public void onCreate() {
